b022b6c56491d2570e8b4282d948877892515cfb
Robert Lancaster <rlancaste@gmail.com>
2017-11-05 17:32:51 -0500

    Adding extra icons and cleaning script

--- setupAstroPi3.sh
+++ setupAstroPi3.sh
@@ -51,11 +51,13 @@ sudo apt-get -y dist-upgrade
 
 # This will set your account to autologin.  If you don't want this. then put a # on each line to comment it out.
 display "Setting account: "$SUDO_USER" to auto login."
+##################
 sudo cat > /usr/share/lightdm/lightdm.conf.d/60-lightdm-gtk-greeter.conf <<- EOF
 [SeatDefaults]
 greeter-session=lightdm-gtk-greeter
 autologin-user=$SUDO_USER
 EOF
+##################
 
 # Installs Synaptic Package Manager for easy software install/removal
 display "Installing Synaptic"
@@ -73,6 +75,25 @@ sudo dpkg -i VNC.deb
 sudo systemctl enable vncserver-x11-serviced.service
 rm VNC.deb
 
+# This will make a folder on the desktop for the launchers
+mkdir ~/Desktop/utilities
+
+# This will create a shortcut on the desktop for creating udev rules for Serial Devices
+##################
+sudo cat > ~/Desktop/utilities/SerialDevices.desktop <<- EOF
+#!/usr/bin/env xdg-open
+[Desktop Entry]
+Version=1.0
+Type=Application
+Terminal=true
+Icon[en_US]=mate-panel-launcher
+Exec=sudo /home/rlancaste/AstroPi3/udevRuleScript.sh
+Name[en_US]=Create Rule for Serial Device
+Name=Create Rule for Serial Device
+Icon=mate-panel-launcher
+EOF
+##################
+
 #########################################################
 #############  Configuration for Hotspot Wifi for Connecting on the Observing Field
 
@@ -81,19 +102,22 @@ rm VNC.deb
 # If Network Manager did not detect internet, it shut down the connections to save energy. 
 # If you want to leave wifi power management enabled, put #'s in front of this section
 display "Preventing Wifi Power Management from shutting down AdHoc and Hotspot Networks"
+##################
 sudo cat > /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf <<- EOF
 [connection]
 wifi.powersave = 2
 EOF
+##################
 
 # This will create a NetworkManager Wifi Hotspot File.  
 # You can edit this file to match your settings now or after the script runs in Network Manager.
 # If you prefer to set this up yourself, you can comment out this section with #'s.
 # If you want the hotspot to start up by default you should set autoconnect to true.
-display "Creating AstroFieldWifi Hotspot Wifi for the observing field"
-sudo cat > /etc/NetworkManager/system-connections/AstroFieldWifi <<- EOF
+display "Creating $(hostname -s)_FieldWifi, Hotspot Wifi for the observing field"
+##################
+sudo cat > /etc/NetworkManager/system-connections/$(hostname -s)_FieldWifi <<- EOF
 [connection]
-id=AstroFieldWifi
+id=$(hostname -s)_FieldWifi
 type=wifi
 autoconnect=false
 interface-name=wlan0
@@ -102,7 +126,7 @@ interface-name=wlan0
 mac-address-blacklist=
 mac-address-randomization=0
 mode=ap
-ssid=AstroFieldWifi
+ssid=$(hostname -s)_FieldWifi
 
 [ipv4]
 dns-search=
@@ -114,18 +138,37 @@ dns-search=
 ip6-privacy=0
 method=ignore
 EOF
+##################
 
 # This will make a link to start the hotspot wifi on the Desktop
-sudo cat > ~/Desktop/StartAstroFieldWifi.desktop <<- EOF
+##################
+sudo cat > ~/Desktop/utilities/StartFieldWifi.desktop <<- EOF
 [Desktop Entry]
 Version=1.0
 Type=Application
 Terminal=false
 Icon[en_US]=mate-panel-launcher
-Name[en_US]=Start AstroFieldWifi
-Exec=nmcli con up AstroFieldWifi
-Name=Start AstroFieldWifi 
+Name[en_US]=Start $(hostname -s) Field Wifi
+Exec=nmcli con up $(hostname -s)_FieldWifi
+Name=Start $(hostname -s)_FieldWifi 
 Icon=mate-panel-launcher
+EOF
+##################
+
+# This will make a link to restart nm-applet which sometimes crashes
+##################
+sudo cat > ~/Desktop/utilities/StartNmApplet.desktop <<- EOF
+[Desktop Entry]
+Version=1.0
+Type=Application
+Terminal=false
+Icon[en_US]=mate-panel-launcher
+Name[en_US]=Restart Network Manager Applet
+Exec=nm-applet
+Name=Restart Network Manager
+Icon=mate-panel-launcher
+EOF
+##################
 
 #########################################################
 #############  File Sharing Configuration
@@ -201,6 +244,7 @@ display "Installing and Configuring INDI Web Manager"
 sudo pip install indiweb
 
 # This will prepare the indiwebmanager.service file
+##################
 sudo cat > /etc/systemd/system/indiwebmanager.service <<- EOF
 [Unit]
 Description=INDI Web Manager
@@ -216,6 +260,7 @@ RestartSec=5
 [Install]
 WantedBy=multi-user.target
 EOF
+##################
 
 # This will change the indiwebmanager.service file permissions and enable it.
 sudo chmod 644 /etc/systemd/system/indiwebmanager.service
@@ -223,14 +268,16 @@ sudo systemctl daemon-reload
 sudo systemctl enable indiwebmanager.service
 
 # This will make a link to the Web Manager on the Desktop
+##################
 sudo cat > ~/Desktop/INDIWebManager.desktop <<- EOF
 [Desktop Entry]
 Encoding=UTF-8
-Name=INDIWebManager
+Name=INDI Web Manager
 Type=Link
 URL=http://localhost:8624
-Icon=text-html
+Icon=/usr/local/lib/python2.7/dist-packages/indiweb/views/img/indi_logo.png
 EOF
+##################
 
 sudo chmod +x ~/Desktop/INDIWebManager.desktop
 sudo chown $SUDO_USER ~/Desktop/INDIWebManager.desktop
