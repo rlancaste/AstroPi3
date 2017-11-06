61f201fa5194696340b83a0bd9e78a3401e4b467
Robert Lancaster <rlancaste@gmail.com>
2017-11-05 21:10:05 -0500

    Improving print statements

--- install_driverRTL8822BU.sh
+++ install_driverRTL8822BU.sh
@@ -1,3 +1,5 @@
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "Attempting to Install Realtek drivers for Wifi Dongle using chipset RTL8822BU"
 if [ "$(whoami)" != "root" ]; then
 	display "Please run this script with sudo due to the fact that it must do a number of sudo tasks.  Exiting now."
 	exit 1
@@ -10,3 +12,5 @@ cd RTL8822BU
 sudo make dkms-install
 make
 sudo make install
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "Script Execution Complete"
