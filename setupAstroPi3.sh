4ca92426d2c8bf7282bdc4b1cf726169cbdbf3c4
Robert Lancaster <rlancaste@gmail.com>
2019-08-29 18:34:43 -0400

    Adding share of whole folder for samba

--- setupAstroPi3.sh
+++ setupAstroPi3.sh
@@ -449,7 +449,30 @@ display "Setting up File Sharing"
 sudo apt -y install samba
 
 # Installs caja-share so that you can easily share the folders you want.
-sudo apt -y install caja-share
+#sudo apt -y install caja-share
+
+# Shares the entire user folder instead (much simpler to work with)
+if [ ! -f /etc/samba/smb.conf ]
+then
+	sudo mkdir -p /etc/samba/
+##################
+sudo cat > /etc/samba/smb.conf <<- EOF
+[global]
+   workgroup = ASTROGROUP
+   server string = Samba Server
+   server role = standalone server
+   log file = /var/log/samba/log.%m
+   max log size = 50
+   dns proxy = no
+[homes]
+   comment = Home Directories
+   browseable = no
+   read only = no
+   writable = yes
+   valid users = $SUDO_USER
+EOF
+##################
+fi
 
 # Adds yourself to the user group of who can use samba, but checks first if you are already in the list
 if [ -z "$(sudo pdbedit -L | grep $SUDO_USER)" ]
@@ -503,6 +526,7 @@ sudo cat > $USERHOME/.config/kdeglobals <<- EOF
 Theme=breeze
 EOF
 ##################
+sudo chown $SUDO_USER $USERHOME/.config/kdeglobals
 
 # Installs the General Star Catalog if you plan on using the simulators to test (If not, you can comment this line out with a #)
 display "Installing GSC"
